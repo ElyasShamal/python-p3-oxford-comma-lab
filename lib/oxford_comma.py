3b941f5c340fd25340f78a1e772320160e798c20
Elyas <Elyas.shamal2@gmail.com>
2023-07-17 20:24:33 -0400

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
-def oxford_comma(items):
-    return None
+
+def oxford_comma(element):
+    if len(element) == 1:
+        return element[0]
+    elif len(element) == 2:
+        return f'{element[0]} and {element[1]}'
+    else:
+        comma_str = ', '.join(element[:-1])
+        return f'{comma_str}, and {element[-1]}'
+
+print(oxford_comma)
